3895fd09100b45282505289293108de04bc6475a
Victor <victor@mail.com>
2026-09-14 11:07:56 +0200

    .

--- hosts/gaming/default.nix
+++ hosts/gaming/default.nix
@@ -49,4 +49,22 @@
     btop
     fastfetch
   ];
+
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "user"
+    ];
+
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://attic.xuyh0120.win/lantian"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+    ];
+  };
+
 }
